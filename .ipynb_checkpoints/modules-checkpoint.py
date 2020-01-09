201dfe6ff1b54b496171ec4137df1ce056ea242d
hslrock <47444387+hslrock@users.noreply.github.com>
2020-01-09 15:47:47 +0900

    update

--- .ipynb_checkpoints/modules-checkpoint.py
+++ .ipynb_checkpoints/modules-checkpoint.py
@@ -96,17 +96,18 @@ class DiscriminatorNet_feature(torch.nn.Module):
         super().__init__()
         self.latent_size=latent_size
         
-        self.fc1 = nn.Linear(latent_size, latent_size*2)
-        self.fc2 = nn.Linear(latent_size*2, latent_size*4)
-        self.fc3 = nn.Linear(latent_size*4, 10)
-        self.bn1 = nn.BatchNorm1d(latent_size*2, 1.e-3)
-        self.bn2 = nn.BatchNorm1d(latent_size*4, 1.e-3)
+        self.fc1 = nn.Linear(latent_size, 1000)
+        self.dout1=nn.Dropout()
+        self.fc2 = nn.Linear(1000, 1000)
+        self.dout2=nn.Dropout()
+        self.fc3 = nn.Linear(1000, 1)
+        self.bn1 = nn.BatchNorm1d(1000, 1.e-3)
+        self.bn2 = nn.BatchNorm1d(1000, 1.e-3)
         self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
-            
+        self.sigmoid=nn.Sigmoid()
     def forward(self,x):
-        x=self.relu(self.bn1(self.fc1(x)))
-        x=self.relu(self.bn2(self.fc2(x)))
+        x=self.relu(self.dout1(self.bn1(self.fc1(x))))
+        x=self.relu(self.dout1(self.bn2(self.fc2(x))))
         return self.sigmoid(self.fc3(x))
         
         
@@ -165,33 +166,3 @@ class GeneratorNet(torch.nn.Module):
         
         return result
 
-class TransNet(torch.nn.Module):
-    def __init__(self,nc,nd,channel1,channel2,channel3):
-        super().__init__()
-        self.fc1=nn.Linear(nc, nd)
-        self.nc=nc
-        self.fcchannel1=nn.Linear(nd,channel1)
-        self.fcchannel2=nn.Linear(nd,channel2)
-        self.fcchannel3=nn.Linear(nd,channel3)
-        self.fc3=nn.Linear(nc,nd)
-        self.fc4=nn.Linear(nd, nc)
-        
-    def disentangle(self,x):
-        z1=self.fcchannel1(x)
-        z2=self.fcchannel2(x)
-        z3=self.fcchannel3(x)
-            
-        return z1,z2,z3
-        
-    def entangle(self,z1,z2,z3):
-        z=torch.cat((z1,z2,z3),1)
-        return self.fc4(self.fc3(z))
-    def forward(self,x):
-        x=x.view(-1,self.nc)   
-        z=self.fc1(x) 
-        z1,z2,z3=self.disentangle(z)
-        z_hat=self.entangle(z1,z2,z3)
-            
-        return z_hat
-            
- 
---
+++ network/transformNet.py
@@ -0,0 +1,43 @@
+import torch
+from torch.autograd import Variable
+from torch import nn
+
+class TransNet(torch.nn.Module):
+    def __init__(self,nc,nd,channel1,channel2,channel3):
+        super().__init__()
+        self.fc1=nn.Linear(nc, nd)
+        self.nc=nc
+        self.fcchannel1=nn.Linear(nd,channel1)
+        self.fcchannel2=nn.Linear(nd,channel2)
+        self.fcchannel3=nn.Linear(nd,channel3)
+        self.fc3=nn.Linear(nc,nd)
+        self.fc4=nn.Linear(nd, nc)
+        
+    def disentangle(self,x):
+        z1=self.fcchannel1(x)
+        z2=self.fcchannel2(x)
+        z3=self.fcchannel3(x)
+            
+        return z1,z2,z3
+        
+    def entangle(self,z1,z2,z3):
+        z=torch.cat((z1,z2,z3),1)
+        return self.fc4(self.fc3(z))
+    def forward(self,x):
+        x=x.view(-1,self.nc)   
+        z=self.fc1(x) 
+        z1,z2,z3=self.disentangle(z)
+        z_hat=self.entangle(z1,z2,z3)
+            
+        return z_hat,z1,z2,z3
+    
+class BCE_Classification(torch.nn.Module):
+    def __init__(self,nc):
+        super().__init__()
+        self.f1=nn.Linear(nc,nc*2)
+        self.f2=nn.Linear(nc*2,1)
+        self.sigmoid=nn.Sigmoid()
+    def forward(self,x):
+        x=self.sigmoid(self.f2(self.f1(x)))
+        return x
+        
