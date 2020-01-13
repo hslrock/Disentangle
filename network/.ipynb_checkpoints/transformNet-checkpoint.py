a01f231c4a74210f6fef054c505c41a307a1a2ea
Hyun Seung Lee <hslrock@gmail.com>
2020-01-13 13:22:02 +0900

    update

--- network/.ipynb_checkpoints/transformNet-checkpoint.py
+++ network/.ipynb_checkpoints/transformNet-checkpoint.py
@@ -2,34 +2,7 @@ import torch
 from torch.autograd import Variable
 from torch import nn
 
-class TransNet(torch.nn.Module):
-    def __init__(self,nc,nd,channel1,channel2,channel3):
-        super().__init__()
-        self.fc1=nn.Linear(nc, nd)
-        self.nc=nc
-        self.fcchannel1=nn.Linear(nd,channel1)
-        self.fcchannel2=nn.Linear(nd,channel2)
-        self.fcchannel3=nn.Linear(nd,channel3)
-        self.fc3=nn.Linear(nc,nd)
-        self.fc4=nn.Linear(nd, nc)
-        
-    def disentangle(self,x):
-        z1=self.fcchannel1(x)
-        z2=self.fcchannel2(x)
-        z3=self.fcchannel3(x)
-            
-        return z1,z2,z3
-        
-    def entangle(self,z1,z2,z3):
-        z=torch.cat((z1,z2,z3),1)
-        return self.fc4(self.fc3(z))
-    def forward(self,x):
-        x=x.view(-1,self.nc)   
-        z=self.fc1(x) 
-        z1,z2,z3=self.disentangle(z)
-        z_hat=self.entangle(z1,z2,z3)
-            
-        return z_hat,z1,z2,z3
+
     
 class BCE_Classification(torch.nn.Module):
     def __init__(self,nc):
@@ -42,34 +15,18 @@ class BCE_Classification(torch.nn.Module):
         return x
         
 class phi(torch.nn.Module):
-    def __init__(self,nc,nd,channel1,channel2,channel3):
+    def __init__(self,nc=33,nd=1000):
         super().__init__()
         self.fc1=nn.Linear(nc, nd)
-        self.nc=nc
-        self.fcchannel1=nn.Linear(nd,channel1)
-        self.fcchannel2=nn.Linear(nd,channel2)
-        self.fcchannel3=nn.Linear(nd,channel3)
+        self.fc2=nn.Linear(nc, nd)
     def disentangle(self,x):
-        z1=self.fcchannel1(x)
-        z2=self.fcchannel2(x)
-        z3=self.fcchannel3(x)
-            
+        z1=x[0:33]
+        z2=x[0:66]
+        z3=x[0:99]
         return z1,z2,z3
     def forward(self,x):
         x=x.view(-1,self.nc)   
         z=self.fc1(x) 
         z1,z2,z3=self.disentangle(z)
+        return 
         
-class invphi():
-    def __init__(self,nc,nd,channel1,channel2,channel3):
-        super().__init__()
-        self.fc1=nn.Linear(nc, nd)
-        self.fc2=nn.Linear(nd, nc)
-        self.nc=nc
-        
-        
-    def entangle(self,z1,z2,z3):
-        z=torch.cat((z1,z2,z3),1)
-        return self.fc4(self.fc3(z))
-    def forward(self,x):
-        return x
