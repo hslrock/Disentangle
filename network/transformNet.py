a13ddc309fc6c2cfc90e306a43d3c86d517a1af3
Hyun Seung Lee <hslrock@gmail.com>
2020-01-14 16:00:50 +0900

    update-lab

--- network/transformNet.py
+++ network/transformNet.py
@@ -4,15 +4,7 @@ from torch import nn
 
 
     
-class BCE_Classification(torch.nn.Module):
-    def __init__(self,nc=33):
-        super().__init__()
-        self.f1=nn.Linear(nc,nc*2)
-        self.f2=nn.Linear(nc*2,1)
-        self.sigmoid=nn.Sigmoid()
-    def forward(self,x):
-        x=self.sigmoid(self.f2(self.f1(x)))
-        return x
+
         
 class phi(torch.nn.Module):
     def __init__(self,nc=99,nd=1000,inv=False):
@@ -36,4 +28,16 @@ class phi(torch.nn.Module):
             z1,z2,z3=self.disentangle(z)
             return z1,z2,z3
         
+class full_phi(torch.nn.Module):    
+    def __init__(self,phi, invphi,nc=99):
         
+        super().__init__()
+        self.nc=nc
+        self.Encoder=phi
+        self.Decoder=invphi
+    def forward(self, x):
+        x=x.view(-1,self.nc) 
+        z = self.Encoder.forward(x)
+        result = self.Decoder.forward(z)
+        
+        return result
